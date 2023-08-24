b8c1c0f247ebe5ec34d5d05206168533c1b79311
Angga <virgi.angga@gmail.com>
2023-08-25 00:19:38 +0700

    update slider

--- js/script.js
+++ js/script.js
@@ -16,6 +16,62 @@ btnClose.addEventListener('click', function() {
   bgDarken.style.display = 'none';
 });
 
+const btnNext = document.getElementById("btn-next");
+const btnPrev = document.getElementById("btn-previous");
+
+const sliderSatu = document.getElementById("slider-satu");
+const sliderDua = document.getElementById("slider-dua");
+const sliderTiga = document.getElementById("slider-tiga");
+
+btnPrev.addEventListener('click', function () {  
+  if(sliderSatu.classList.contains('order-1')){
+    sliderSatu.classList.remove("order-1");
+    sliderSatu.classList.add("order-3");
+    sliderDua.classList.remove("order-2");
+    sliderDua.classList.add("order-1");
+    sliderTiga.classList.remove("order-3");
+    sliderTiga.classList.add("order-2");
+  } else if(sliderSatu.classList.contains('order-2')){
+    sliderSatu.classList.remove("order-2");
+    sliderSatu.classList.add("order-1");
+    sliderDua.classList.remove("order-3");
+    sliderDua.classList.add("order-2");
+    sliderTiga.classList.remove("order-1");
+    sliderTiga.classList.add("order-3");
+  } else if(sliderSatu.classList.contains('order-3')){
+    sliderSatu.classList.remove("order-3");
+    sliderSatu.classList.add("order-2");
+    sliderDua.classList.remove("order-1");
+    sliderDua.classList.add("order-3");
+    sliderTiga.classList.remove("order-2");
+    sliderTiga.classList.add("order-1");
+  }
+});
+
+btnNext.addEventListener('click', function () {  
+  if(sliderSatu.classList.contains('order-1')){
+    sliderSatu.classList.remove("order-1");
+    sliderSatu.classList.add("order-2");
+    sliderDua.classList.remove("order-2");
+    sliderDua.classList.add("order-3");
+    sliderTiga.classList.remove("order-3");
+    sliderTiga.classList.add("order-1");
+  } else if(sliderSatu.classList.contains('order-2')){
+    sliderSatu.classList.remove("order-2");
+    sliderSatu.classList.add("order-3");
+    sliderDua.classList.remove("order-3");
+    sliderDua.classList.add("order-1");
+    sliderTiga.classList.remove("order-1");
+    sliderTiga.classList.add("order-2");
+  } else if(sliderSatu.classList.contains('order-3')){
+    sliderSatu.classList.remove("order-3");
+    sliderSatu.classList.add("order-1");
+    sliderDua.classList.remove("order-1");
+    sliderDua.classList.add("order-2");
+    sliderTiga.classList.remove("order-2");
+    sliderTiga.classList.add("order-3");
+  }
+});
 
 
 
